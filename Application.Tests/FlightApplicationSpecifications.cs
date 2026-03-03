6290c442765d8a34573941281af91e549483f7fd
Hanré Koen <601225@student.belgiumcampus.ac.za>
2026-03-03 23:46:10 +0200

    commit

--- Application.Tests/FlightApplicationSpecifications.cs
+++ Application.Tests/FlightApplicationSpecifications.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Data;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using Application.Tests;
 
 namespace Application.Tests
 {
@@ -32,43 +33,10 @@ namespace Application.Tests
         }
     }
 
-    public class BookingService
-    {
-        public BookingService(Entities entities)
-        {
-            
-        }
+}
 
-        public void Book(BookDto bookDto)
-        {
 
-        }
-
-        public IEnumerable<BookingRm> FindBookings(Guid flightId)
-        {
-            return new[]
-            {
-                new BookingRm(passengerEmail: "a@b.com", numberOfSeats: 2)
-            };
-        }
-    }
-    public class BookDto //Data transfer object
-    {
-        public BookDto(Guid flightId, string passengerEmail, int numberOfSeats)
-        {
-            
-        }
-    }
-
-    public class BookingRm //Read model
-    {
-        public string PassengerEmail { get; set; }
-        public int NumberOfSeats { get; set; }
-        public BookingRm(string passengerEmail, int numberOfSeats)
-        {
-            this.PassengerEmail = passengerEmail;
-            this.NumberOfSeats = numberOfSeats;
-        }
-    }
+namespace Application.Tests
+{
 
 }
---
+++ Application/BookDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class BookDto //Data transfer object
+    {
+        public Guid FlightId { get; set; }
+        public string PassengerEmail { get; set; }
+        public int NumberOfSeats { get; set; }
+        public BookDto(Guid flightId, string passengerEmail, int numberOfSeats)
+        {
+            FlightId = flightId;
+            PassengerEmail = passengerEmail;
+            NumberOfSeats = numberOfSeats;
+        }
+    }
+
+}
